0bacc2eabfa41dba089a972d9b2eb85419854643
Marcio <marcio@draimo.com>
2020-10-16 20:03:43 -0300

    Cambio de configuracion en package.json para el deploy en heroku

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     appConfig:{
         host: process.env.APP_HOST,
-        port: process.env.APP_PORT
+        port: process.env.PORT
     },
     jwtConfig: {
         SECRET_TOKEN: 'miclavedetokens' // el codigo deberia ser mas largo (esto es de prueba)
